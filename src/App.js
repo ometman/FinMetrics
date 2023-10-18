9cd65983e7b6915a1fe90d53729e4cd9b48b4fc9
ometman <revomet@gmail.com>
2023-10-18 13:17:21 +0000

    Modify App.js, index.js for store

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import {
+  Routes,
+  Route,
+  BrowserRouter,
+} from 'react-router-dom';
+import NavLink from './routes/NavLink';
+import Home from './routes/Home';
+// import Profile from './routes/Profile';
+import DetailsPage from './routes/DetailsPage';
+import ErrorPage from './routes/ErrorPage';
 import './App.css';
 
 function App() {
   return (
-    <div>Welcome to React Capstone</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NavLink />}>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/details" element={<DetailsPage />} />
+          {/* <Route path="/profile" element={<Profile />} /> */}
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
